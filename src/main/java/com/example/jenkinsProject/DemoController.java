19ee72f29e96585c1e588d7f87db72721c9b0662
evanjalinkirthika <evanjalin.muthuraj@thoughtworks.com>
2021-11-01 10:26:08 +0530

    Jenkins file changes

---
+++ src/main/java/com/example/jenkinsProject/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.jenkinsProject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class DemoController {
+
+    @RequestMapping(value = "hello")
+    public String hello(){
+        return "Spring boot Gradle Example";
+    }
+}
